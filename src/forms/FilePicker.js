95eaa30e5921c903fc1131769e6fa899501f03e0
Colin McGraw <colinmcgraw@me.com>
2018-12-04 16:48:07 -0800

    filepicker image preview

--- src/forms/FilePicker.js
+++ src/forms/FilePicker.js
@@ -20,6 +20,8 @@ class FilePicker extends React.Component {
     this._onDragEnter = this.onDragEnter.bind(this)
     this._onDragLeave = this.onDragLeave.bind(this)
     this._onDropzoneClick = this.onDropzoneClick.bind(this)
+    this._onDropzoneDrop = this.onDropzoneDrop.bind(this)
+    this._onInputChange = this.onInputChange.bind(this)
   }
 
   onDragEnter(e) {
@@ -39,6 +41,41 @@ class FilePicker extends React.Component {
     e.preventDefault()
     this.inputRef.current.click()
   }
+
+  onDropzoneDrop(e) {
+    e.stopPropagation()
+    e.preventDefault()
+    this.handleFiles(e.dataTransfer.files)
+  }
+
+  onInputChange(e) {
+    e.preventDefault()
+    this.handleFiles(e.target.files)
+  }
+  
+
+  getImagePreview(file, callback) {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = () => callback(reader.result)
+  }
+
+  handleFiles(files) {
+    if (files.length === 0) return
+    this.setState({ loading: true })
+
+    console.log('Files: ', files)
+
+    for (let i=0; i < files.length; i++) {
+      this.getImagePreview(files[i], preview => {
+        const newfile = [{ file: files[i], preview }]
+        this.setState({
+          files: [...this.state.files, ...newfile]
+        })
+      })
+    }
+  }
+
 
   render() {
     const { id, label, nameAttr, multiple, disabled, accept, dropzone, errors } = this.props
@@ -68,12 +105,13 @@ class FilePicker extends React.Component {
           {files.length === 0 &&
           <span>No Files Chosen...</span>}
 
-          <input type="file" id={id} ref={this.inputRef} accept={accept} multiple={multiple} />
+          <input type="file" id={id} ref={this.inputRef} accept={accept} multiple={multiple} 
+           onChange={e => this._onInputChange(e)} />
         </div>
         
         {dropzone &&
-        <div className={dropzoneClasses} onDragOver={e => this._onDragOver} onClick={e => this._onDropzoneClick(e)}
-         onDragEnter={e => this._onDragEnter(e)} onDragLeave={(e) => this._onDragLeave(e)}>
+        <div className={dropzoneClasses} onDragOver={e => this._onDragOver(e)} onClick={(e) => this._onDropzoneClick(e)}
+         onDragEnter={e => this._onDragEnter(e)} onDragLeave={(e) => this._onDragLeave(e)} onDrop={(e) => this._onDropzoneDrop(e)}>
           
           {files.length === 0 &&
           <div className="mag-dropzone-empty">
@@ -81,8 +119,15 @@ class FilePicker extends React.Component {
           </div>}
 
           {files.length > 0 &&
-          <ul>
+          <ul className="mag-filepicker-files">
+            {files.map((file, i) => 
+            <li key={i}>
+              <figure>
+                <img src={file.preview} alt={file.file.title} />
+              </figure>
+            </li>)}
           </ul>}
+
         </div>}
         
        
