0c30249b0ee31367a37cba43ebab362172886766
Colin McGraw <colinmcgraw@me.com>
2018-12-17 09:39:02 -0800

    flex components

--- src/App2.js
+++ src/App2.js
@@ -1,6 +1,6 @@
-// This Is An Example Implementation of the UI Kit
 import React from 'react'
 
+// This Is An Example Implementation of the UI Kit
 import Layout from './layout/Layout'
 import TopBar from './layout/TopBar'
 import SideBar from './layout/SideBar'
@@ -19,7 +19,19 @@ class App extends React.Component {
       <Layout fullscreen={true} sidebar={true} sidebarOpen={false}>
         
         <TopBar fixed={true}>
-          
+          <FlexItem className="mag-topbar--brand">
+            <h3>Magnetic Components</h3>
+          </FlexItem>
+          <FlexItem className="mag-topbar--nav">
+            <ul>
+              <li>
+                <a href="#0">Account</a>
+              </li>
+              <li>
+                <a href="#0">Dashboard</a>
+              </li>
+            </ul>
+          </FlexItem>
         </TopBar>
 
         <SideBar>
@@ -30,7 +42,7 @@ class App extends React.Component {
 
           <Page>
 
-            <FilePicker id="filepicker" label="Choose An Image" dropzone={true} />
+            <FilePicker id="filepicker" label="Choose An Image" dropzone={true} onFiles={} />
 
             <div style={{height: '3em'}}></div>
 
